b527563ad5d5727d94ae69fc46e0f74c12dd2d0a
sevenkeed7 <148210333+sevenkeed7@users.noreply.github.com>
2026-04-17 14:39:44 +0300

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://sactjhvgtycnfahhsfsw.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhY3RqaHZndHljbmZhaGhzZnN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTk1MzYsImV4cCI6MjA4MTU3NTUzNn0.BUY8aOywqZfOdk4yspZVhGostiETmomGfaDRxhzfztE'
+const supabaseUrl = 'https://khqjmfqiozratcefcbrn.supabase.co'
+const supabaseAnonKey = 'sb_publishable_u79xXjgd8Wx9QeC49P_6CQ_fawnLvPg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
